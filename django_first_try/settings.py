3b245ef68d3ad64bce9a87dadbb363d5bcf76fad
sinoy <sinu.study.04112512@gmail.com>
2023-11-05 13:00:29 +0530

    made registration option

--- django_first_try/settings.py
+++ django_first_try/settings.py
@@ -125,3 +125,6 @@ CRISPY_TEMPLATE_PACK = 'bootstrap5'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'blog-homepage'
+#LOGIN_URL = 'login'
--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@
 from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegisteraationForm
 
 def register(request):
@@ -10,9 +11,16 @@ def register(request):
         if form.is_valid():
             username = form.cleaned_data.get('username')
             form.save()
-            messages.success(request, f'Account created for {username}! ')
+            messages.success(request, f'Account created for {username}!,You can now Login')
 
-            return redirect('blog-homepage')
+            return redirect('login')
     else:
         form = UserRegisteraationForm()
     return render(request, 'users/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    return render (request,'users/profile.html')
+
+
+                   
